54d9fbdf35edffaa8b64b55238eb82080d700245
dacortes <dannyel.cortesr@gmail.com>
2024-01-30 11:03:00 +0100

    [IMP] to_string ex01

--- ex01/src/zombieHorde.cpp
+++ ex01/src/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: dacortes <dacortes@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 09:41:55 by dacortes          #+#    #+#             */
-/*   Updated: 2024/01/29 10:48:59 by dacortes         ###   ########.fr       */
+/*   Updated: 2024/01/30 11:02:10 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,13 @@
 
 Zombie	*zombieHorde(int N, std::string name)
 {
-	char	num;
-	Zombie *foo;
+	std::string	num;
+	Zombie	*foo;
 
-	num = '0';
 	foo = new Zombie[N];
 	for (int i = 0; i < N; i++)
 	{
-		num++;
+		num = std::to_string(i + 1);
 		foo[i].setName(std::string(name) + num);
 	}
 	return (foo);
