b0bd289d4752b470804f36d8002035d998ec4384
dacortes <dannyel.cortesr@gmail.com>
2024-02-01 16:14:29 +0100

    [IMP] NOT IF CRACK

--- ex05/src/main.cpp
+++ ex05/src/main.cpp
@@ -6,18 +6,31 @@
 /*   By: dacortes <dacortes@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 18:04:40 by dacortes          #+#    #+#             */
-/*   Updated: 2024/02/01 11:47:33 by dacortes         ###   ########.fr       */
+/*   Updated: 2024/02/01 16:10:04 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/Harl.h"
 
-int	main(void)
+bool	msg_error(void)
+{
+	std::cout << "Error: invalid arguments" << std::endl; 
+	return (EXIT_FAILURE);
+}
+
+bool check_input(std::string str)
+{
+	return (!str.compare("DEBUG") || !str.compare("INFO")
+			|| !str.compare("WARNING") || !str.compare("ERROR"));
+}
+
+int	main(int ac, char **av)
 {
 	Harl	two;
-	two.complain("DEBUG");
-	two.complain("INFO");
-	two.complain("WARNING");
-	two.complain("ERROR");
+
+	while (ac != 2 || !av[1] || !*av[1]
+			|| (av[1] && *av[1] && !check_input(av[1])))
+		return (msg_error());
+	two.complain(av[1]);
 	return (EXIT_SUCCESS);
 }
